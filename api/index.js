e6a3d7850a9bccb6722967b69fb1fef13f12e80e
Amirul Hossain <amirul1709@hotmail.com>
2025-04-25 13:44:36 -0600

    backend integration

---
+++ api/index.js
@@ -0,0 +1,13 @@
+import express from "express"
+
+const app = express();
+
+app.get("/api/test", (req, res) => {
+    res.json("test ok");
+});
+
+app.post("api/transaction", (req, res) => {
+    res.json(req.body);
+});
+
+app.listen(3000);
